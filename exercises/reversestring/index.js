f2f68af02202629a5e68e09d2fe928ad0c54c8fb
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-10-14 12:34:10 -0300

    Add solution and code formatter

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,25 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//   const chars = str.split("");
+//   const length = str.length;
+//   const reversed = []
+//
+//   for (var i = length - 1; i >= 0 ; i--) {
+//     reversed.push(chars[i])
+//   }
+//
+//   return reversed.join("");
+// }
 
+function reverse(str) {
+  debugger
+  return str
+    .split("")
+    .reverse()
+    .join("");
+}
+
+reverse("haa");
 module.exports = reverse;
