1067992fc3c992d11a9bbfb0db4dd31dd75ab97b
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-10-14 14:30:06 -0300

    Add chunks solution

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,18 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const chunks = [];
+
+  array.forEach((element, index) => {
+    if (index % size == 0) {
+      chunks.push([]);
+    }
+
+    chunks[chunks.length - 1].push(element);
+  });
+
+  return chunks;
+}
 
 module.exports = chunk;
