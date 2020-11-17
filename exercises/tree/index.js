1489f42f5a38c97f6c80917ba45dacf8f847cbaf
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2020-11-17 08:19:36 -0300

    Implement part of the tree

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,34 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data
+    this.children = []
+  }
 
-class Tree {}
+  add(data) {
+    this.children.push(new Node(data))
+  }
+
+  remove(data) {
+    this.children = this.children.filter(node => node.data  !== data)
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null
+  }
+
+  traverseBF(callback) {
+    const queue = [this.root];
+    while(queue.length > 0) {
+      const current = queue.shift();
+      current.children.forEach(child => queue.push(child));
+      callback(current);
+    }
+  }
+}
 
 module.exports = { Tree, Node };
